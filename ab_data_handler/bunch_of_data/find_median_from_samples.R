88e18dc193cdb55d6b0b27beb6b0c81c8be99ceb
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-12-13 13:52:32 -0200

    Refactored median function

--- ab_data_handler/bunch_of_data/find_median_from_samples.R
+++ ab_data_handler/bunch_of_data/find_median_from_samples.R
@@ -4,8 +4,14 @@
 # published by the Free Software Foundation.
 
 # Capture samples
+# @param path Path to a directory with all csvs files
 list_of_samples <- function(path)
 {
+  if (!dir.exists(path))
+  {
+    stop("Please, inform a valid directory")
+  }
+
   # Handling files
   file_samples <- list.files(path, pattern='*.csv', full.names = FALSE)
   file_numbers <- as.numeric(sub('\\.csv$', '', file_samples))
@@ -16,55 +22,55 @@ list_of_samples <- function(path)
   return(all_files)
 }
 
-# Based on a folder with samples (csv files), read all of them and find out
-# the median
-find_median_from_samples <- function(path)
+# Based on a folder with samples (csv files), read all of them and calculate
+# median
+# @param save_to calculated median to
+find_median_from_samples <- function(save_to)
 {
-  all_files <- list_of_samples(path)
+  all_files <- list_of_samples(save_to)
 
   # Set up variables
   total_lines <- nrow(all_files[[1]]) # Expected 101
-  total_files <- length(all_files) # Total of samples, it can vary
+  total_files <- length(all_files) # Total of samples. It can vary
   final_table <- data.frame(Percentage=numeric(), Time=numeric())
 
   # Go through each line
   for (lines in 1:total_lines)
   {
-    tmprow <- data.frame(Percentage=numeric(), Time=numeric())
+    tmp_row <- data.frame(Percentage=numeric(), Time=numeric())
     # Cross all files, and get each column value
     for (target in 1:total_files)
     {
       sample_time <- all_files[[target]][lines, 2]
       value_sample <- data.frame(Percentage=lines, Time=sample_time)
-      tmprow <- rbind(tmprow, value_sample)
+      tmp_row <- rbind(tmp_row, value_sample)
     }
     # Find median
-    calculed_median <- median(tmprow$Time)
+    calculed_median <- median(tmp_row$Time)
     newrow <- data.frame(Percentage=lines, Time=calculed_median)
     final_table <- rbind(final_table, newrow)
   }
-
   return (final_table)
 }
 
-process_data_median <- function(path, dest)
+process_data_median <- function(read_files_from, save_to)
 {
-  table_of_median <- find_median_from_samples(path)
-  write.csv(table_of_median, file=dest)
+  table_of_median <- find_median_from_samples(read_files_from)
+  write.csv(table_of_median, file=save_to)
 }
 
 # Read arguments
-pathsArguments <- commandArgs(trailingOnly=TRUE)
+paths_arguments <- commandArgs(trailingOnly=TRUE)
 
 # Test if there three argument. If not, return an error.
-if (length(pathsArguments) < 2)
+if (length(paths_arguments) < 2)
 {
   stop('You have to supply the target path and destination')
-} else if (length(pathsArguments) == 2)
+} else if (length(paths_arguments) == 2)
 {
   # default output file
-  source_files <- pathsArguments[1]
-  dest_files <- pathsArguments[2]
+  source_files <- paths_arguments[1]
+  dest_files <- paths_arguments[2]
 }
 
 process_data_median(source_files, dest_files)
--- ab_data_handler/graphs/box_plot.R
+++ ab_data_handler/graphs/box_plot.R
@@ -4,72 +4,59 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
 # published by the Free Software Foundation.
+source(file="util/flexible_parameters.R")
 
-# Load CSV file, and return data.frame structure
-load_csv_file <- function(path_csv)
+load_tsv_file <- function(path_tsv)
 {
-  data <- read.csv(file=path_csv, skip=1,
-                   col.names=c('Percentage','Time'))
-  return(data)
+  labels <- c('starttime', 'seconds', 'ctime', 'dtime', 'ttime', 'wait')
+  
+  if(!file.exists(path_tsv))
+  {
+    stop(paste('File does not exist: ', path_csv))
+  }
+  loaded_data <- read.table(file=path_tsv, col.names=labels, sep='\t')
+
+  return(loaded_data)
 }
 
 # Create boxplot of data visualization
 # @param increase_rate Increase rate to use in x axis
 # @param base_path base path to read data
 # @param dest_path path to save data
-boxplot_visualization <- function(increase_rate, base_path, dest_path)
+boxplot_visualization <- function(graph_name, values_to_plot)
 {
-  label_x <- seq(from=increase_rate, by=increase_rate, length.out=10)
-  column <- c('NULL', 'NULL', 'NULL', NA, 'NULL')
-  options(scipen=100) # FIXME: Change the expansion. Not a good idea
+  names_line <- names(values_to_plot)
+  total_elements <- length(values_to_plot)
+  set_of_data <- c()
+  colors <- c('blue', 'green', 'red', 'orange', 'black', 'grey', 'navy')
 
-  for (mpm in c('event', 'worker', 'prefork'))
+  # Load data, find max value for X and create label variables
+  invisible_box <- data.frame()
+  for (line in names_line)
   {
-    output_name <- paste(mpm, 'boxplot.png', sep='_')
-    current_dest_path <- paste(dest_path, '/', output_name, sep='')
-    png(current_dest_path, width=1024, height=768)
-
-    boxes <- list(1:10)
-
-    for (position in 1:10)
-    {
-      current_file <- paste(label_x[position], mpm, sep='_')
-      current_file <- paste(base_path,'/', current_file, '.csv', sep='')
-      boxes[[1]][position] <- read.csv(file=current_file, colClasses=column)
-    }
-
-    boxplot(boxes[[1]][[1]], boxes[[1]][[2]],
-            boxes[[1]][[3]], boxes[[1]][[4]],
-            boxes[[1]][[5]], boxes[[1]][[6]],
-            boxes[[1]][[7]], boxes[[1]][[8]],
-            boxes[[1]][[9]], boxes[[1]][[10]],
-            las=2, names=label_x)
-    mtext('Average time (ms)', side=2, line=3)
-    mtext('Requests', side=1, line=4)
-    title(main=mpm)
-    dev.off()
+    loaded_file <- load_tsv_file(values_to_plot[[line]])
+    set_of_data[[line]] <- loaded_file
+    l <- data.frame(line = c(loaded_file$ttime))
+    invisible_box <- rbind(invisible_box, l)
   }
+  png(graph_name, width=1024, height=768)
+
+  boxplot(invisible_box, xlim=c(0.5, total_elements - 0.5),
+          boxfill=rgb(1, 1, 1, alpha=1), border=rgb(1, 1, 1, alpha=1),
+          xaxt="n", las=2)
+
+  position <- 1
+  for (key in names_line)
+  {
+    current_data <- set_of_data[[key]]
+    boxplot(as.numeric(current_data$ttime), add=TRUE, boxwex=0.45, at=position,
+            las=2, xaxt="n")
+    position <- position + 0.5
+  }
+  mtext('Average time (ms)', side=2, line=3)
+  dev.off()
   return (0)
 }
 
 # Read arguments
-pathsArguments <- commandArgs(trailingOnly=TRUE)
-
-# Test if there three argument. If not, return an error.
-if (length(pathsArguments) < 4)
-{
-  stop('You have to supply the path for each mpm strategy')
-} else if (length(pathsArguments) == 4)
-{
-  # default output file
-  graph_name <- pathsArguments[1]
-  event_file <- pathsArguments[2]
-  worker_file <- pathsArguments[3]
-  prefork_file <- pathsArguments[4]
-}
-
-event <- load_csv_file(event_file)
-worker <- load_csv_file(worker_file)
-prefork <- load_csv_file(prefork_file)
-
-boxplot_visualization(graph_name, event, worker, prefork)
+boxplot_visualization(save_to, parameters)
